17d6922ff0ad72ea6946c81a7842396bc5cda9fd
ilvmikan <mattdias30@gmail.com>
2024-01-25 15:16:32 -0300

    feat(mod) criando checagem de permissões e estrutura dos comandos

--- cogs/admin.py
+++ cogs/admin.py
@@ -21,74 +21,66 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(name='mod')
+    async def check_permissions(self, ctx, action):
+        required_permissions = discord.Permissions()
+        required_permissions.update(**{action: True})
+
+        if not ctx.guild.me.guild_permissions >= required_permissions or not ctx.author.guild_permissions >= required_permissions:
+            await ctx.send(f'Você não tem permissão para usar esse comando')
+            return False
+        return True
+
+    async def perform_action(self, ctx, action, user, reason=None):
+        try:
+            method = getattr(ctx.guild, action)
+            await method(user, reason=reason)
+            await ctx.send(f'{user} foi {action}, razão: {reason}')
+        except discord.Forbidden:
+            await ctx.send(f'Sem permissão para {action} {user}')
+
+    @commands.group(name='mod', invoke_without_command=True)
     async def _mod(self, ctx):
         await ctx.channel.send('!mod <command>')
 
     @_mod.command(name='kick')
     async def _kick(self, ctx, user: discord.Member, *, reason=None):
-        if ctx.guild.me.guild_permissions.kick_members and ctx.author.guild_permissions.kick_members:
-            try:
-                await user.kick(reason=reason)
-                await ctx.send(f'{user} foi expulso, razão: {reason}')
-            except discord.Forbidden:
-                await ctx.send(f'Sem permissão para expulsar {user}')
-        else:
-            await ctx.send(f'Sem permissão para expulsar membros')
+        if await self.check_permissions(ctx, 'kick_members'):
+            await self.perform_action(ctx, 'kick', user, reason)
 
     @_mod.command(name='ban')
-    async def _ban(ctx, user: discord.Member, *, reason=None):
-        if ctx.guild.me.guild_permissions.ban_members and ctx.author.guild_permissions.ban_members:
-            try:
-                await user.ban(reason=reason)
-                await ctx.send(f'{user} foi banido, razão: {reason}')
-            except discord.Forbidden:
-                await ctx.send(f'Sem permissão para expulsar {user}')
-        else:
-            await ctx.send(f'Sem permissão para banir membros')
+    async def _ban(self, ctx, user: discord.Member, *, reason=None):
+        if await self.check_permissions(ctx, 'ban_members'):
+            await self.perform_action(ctx, 'ban', user, reason)
 
     @_mod.command(name='unban')
-    async def _unban(ctx, user_id: int, reason=None):
-        if ctx.author.guild_permissions.ban_members and ctx.guild.me.guild_permissions.ban_members:
-            try:
-                await ctx.guild.unban(discord.Object(id=user_id), reason=reason)
-                await ctx.send(f'Usuário com ID {user_id} foi desbanido, razão: {reason}')
-            except discord.Forbidden:
-                await ctx.send(f'Sem permissão para desbanir usuário com ID {user_id}')
-        else:
-            await ctx.send('Sem permissão para desbanir.')
+    async def _unban(self, ctx, user_id: int, reason=None):
+        if await self.check_permissions(ctx, 'ban_members'):
+            user = discord.Object(id=user_id)
+            await self.perform_action(ctx, 'unban', user, reason)
 
     @_mod.command(name='mute')
-    async def mute(ctx, member: discord.Member, duration=None, *, reason=None):
-        if ctx.author.guild_permissions.mute_members:
-            muted_role = discord.utils.get(ctx.guild.roles, name='Muted')
+    async def mute(self, ctx, member: discord.Member, duration=None, *, reason=None):
+        if await self.check_permissions(ctx, 'mute_members'):
+            await self.handle_mute(ctx, member, duration, reason)
 
-            if not muted_role:
-                muted_role = await ctx.guild.create_role(name='Muted')
+    async def handle_mute(self, ctx, member, duration, reason):
+        muted_role = discord.utils.get(ctx.guild.roles, name='Muted')
 
-                for channel in ctx.guild.channels:
-                    await channel.set_permissions(muted_role, speak=False, send_messages=False)
+        if not muted_role:
+            muted_role = await ctx.guild.create_role(name='Muted')
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(muted_role, speak=False, send_messages=False)
 
-            await member.add_roles(muted_role, reason=reason)
+        await member.add_roles(muted_role, reason=reason)
 
-            if duration:
-                time.sleep(parse_duration(duration))
-                await member.remove_roles(muted_role, reason="Tempo de mute expirado")
+        if duration:
+            await discord.utils.sleep(parse_duration(duration))
+            await member.remove_roles(muted_role, reason="Tempo de mute expirado")
 
-            await ctx.send(f'{member.mention} foi mutado.')
+        await ctx.send(f'{member.mention} foi mutado.')
 
-        else:
-            await ctx.send('Você não tem permissão para mutar membros.')
-    
     @_mod.command(name='unmute', hidden=True)
-    async def _unmute(ctx, user: discord.Member):
-        """
-        Desmuta um usuário específico
-
-        args:
-            ctx: O contexto da mensagem que acionou o comando.
-            user: um objeto do usuário que foi mencionado para ser silenciado
-        """
+    async def _unmute(self, ctx, user: discord.Member):
         muted_role = discord.utils.get(ctx.guild.roles, name='Muted')
 
         if muted_role in user.roles:
@@ -100,20 +92,9 @@ class Admin(commands.Cog):
         else:
             await ctx.send(f'{user.mention} não possui o cargo "Muted".')
 
+    @commands.has_guild_permissions(manage_messages=True)
     @_mod.command(name='clear', hidden=True)
-    async def _clear(ctx, quantity: int):
-        """
-        Exclui as mensagens de um canal onde o comando "!mod clear <quantidade>" foi chamado
-        
-        args:
-            ctx (discord.ext.commands.Context): O contexto da mensagem que acionou o comando.
-            quantity: quantidade de mensagens para serem excluídas.
-        """
-        msg = []
-        async for message in ctx.channel.history(limit=quantity + 1):
-            msg.append(message)
-    
-        await ctx.channel.delete_messages(msg)
-        await ctx.send(f'{quantity} mensagens foram excluídas')
+    async def _clear(self, ctx, quantity: int):
+        messages = await ctx.channel.purge(limit=quantity + 1)
+        await ctx.send(f'{len(messages) - 1} mensagens foram excluídas')
         print(f'As mensagens do canal "{ctx.channel.name}" (ID: {ctx.channel.id}) do servidor "{ctx.guild.name}" (ID: {ctx.guild.id}) foram excluídas')
-    
