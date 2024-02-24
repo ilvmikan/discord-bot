44c93cf3b154f81e8476cf40c8a07de986597f2e
ilvmikan <mattdias30@gmail.com>
2024-02-24 12:23:08 -0300

    add edit color and profile config

--- database/db.py
+++ database/db.py
@@ -34,6 +34,9 @@ class UserProfile:
         self.db = Database(db_name)
         self.db.connect()
 
+    """
+        MELHORAR O CÓDIGO DEPOIS, PELO MENOS FUNCIONA
+    """
 
     def create_table(self):
         query = """
@@ -41,13 +44,27 @@ class UserProfile:
             id INTEGER PRIMARY KEY,
             id_discord TEXT UNIQUE,
             description TEXT,
-            background_image TEXT'
+            background_image TEXT,
+            color_red INTEGER DEFAULT 0,
+            color_green INTEGER DEFAULT 0,
+            color_blue INTEGER DEFAULT 255
         )
         """
         self.db.execute_query(query)
 
 
-    def create_user_profile(self, id_discord, description=None, image_url=None):
+    def delete_table_content(self):
+        delete_query = "DELETE FROM users"
+        self.db.execute_query(delete_query)
+        
+        num_rows_affected = self.db.cursor.rowcount
+
+        if num_rows_affected > 0:
+            return 'Deletado!'
+        return 'Erro ao deletar'
+
+
+    def create_user_profile(self, id_discord, description=None, image_url=None, color=(0, 0, 255)):
         query_check = "SELECT id_discord FROM users WHERE id_discord = ?"
         values_check = (id_discord,)
         self.db.cursor.execute(query_check, values_check)
@@ -56,23 +73,31 @@ class UserProfile:
             print("Usuário já existe na tabela.")
             return False
 
-        query_insert = "INSERT INTO users (id_discord, description, background_image) VALUES (?, ?, ?)"
-        values_insert = (id_discord, description, image_url)
+        query_insert = """
+            INSERT INTO users (id_discord, description, background_image, color_red, color_green, color_blue)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """
+        values_insert = (id_discord, description, image_url, color[0], color[1], color[2])
         self.db.execute_query(query_insert, values_insert)
-        print("Perfil do usuário criado com sucesso.")
+        print("Perfil do usuário criado com sucesso!!")
 
 
     def show_user_profile(self, user_id):
-        query = "SELECT description, background_image FROM users WHERE id_discord = ?"
+        query = "SELECT description, background_image, color_red, color_green, color_blue FROM users WHERE id_discord = ?"
         values = (user_id,)
 
         self.db.cursor.execute(query, values)
         result = self.db.cursor.fetchone()
+
         if result:
-            return result[0], result[1]
+            return result[0], result[1], (result[2], result[3], result[4])
         else:
-            return "Descrição não encontrada no banco de dados.", "https://i.pinimg.com/564x/b1/90/a2/b190a2ac5f6912ff2d976f3c753c0331.jpg"
-    
+            return (
+                "Utilize !profile create <desc> <img_url> para criar seu perfil",
+                "https://i.pinimg.com/564x/b1/90/a2/b190a2ac5f6912ff2d976f3c753c0331.jpg",
+                (0, 0, 255)
+            )
+
 
     def edit_description(self, user_id, new_description):
         query_update = "UPDATE users SET description = ? WHERE id_discord = ?"
@@ -87,6 +112,25 @@ class UserProfile:
         values_update = (new_image, user_id)
         self.db.execute_query(query_update, values_update)
         print("Descrição do usuário atualizada com sucesso.")
+
+
+
+    def edit_color(self, user_id, r, g, b):
+        query_update = "UPDATE users SET color_red = ?, color_green = ?, color_blue = ? WHERE id_discord = ?"
+        values_update = (r, g, b, user_id)
+        self.db.execute_query(query_update, values_update)
+        print("Cor do usuário atualizada com sucesso.")
+
+    
+    def profile_config(self, user_id):
+        query = "SELECT id_discord, description, background_image, color_red, color_green, color_blue FROM users WHERE id_discord = ?"
+        values = (user_id,)
+
+        self.db.cursor.execute(query, values)
+        result = self.db.cursor.fetchone()
+
+        return result
+
 
 
 
